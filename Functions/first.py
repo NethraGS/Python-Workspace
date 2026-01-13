59232ae0336b968f772f156df8a908d49a922cf0
NethraGS <nethra120304@gmail.com>
2026-01-13 15:53:24 +0530

    functions exercise

--- Functions/first.py
+++ Functions/first.py
@@ -1,1 +1,10 @@
-print("hello")
+def greet_people(name,rollno):
+    print("hello",name,"your roll number is",rollno)
+greet_people("neo",130)
+
+def sum(a,b):
+    return a+b
+print("sum is ",sum(10,50))
+a1="10"
+b1="20"
+print(a1+b1)
